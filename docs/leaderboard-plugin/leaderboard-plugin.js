0c69e12dee9da25b3113c5594b6b53b23c117808
Felix Schlegel <felix.schlegel@gmx.de>
2023-06-27 22:48:44 +0200

    feat: Added leaderboard plugin

---
+++ docs/leaderboard-plugin/leaderboard-plugin.js
@@ -0,0 +1,199 @@
+(function () {
+    var leaderboardPlugin = function (hook, vm) {
+        hook.beforeEach(
+            /** @param {string} markdown */
+            function (markdown) {
+                if (!markdown.includes("{{leaderboard}}")) {
+                    return markdown;
+                }
+
+                /**
+                 * @type {{rating: {firstPlace: number, secondPlace: number, thirdPlace: number}, players: {name: string, singleParticipations: number, teamParticipations: number}[], turnaments: {date: string, flavor: string, type: string, firstPlace: string[], secondPlace: string[], thirdPlace: string[]}[]}}
+                 */
+                const data = turnamentData;
+
+                // Calculate points and metrics
+                const team_leaderboard = fillLeaderboardData(data, TurnamentType.Team);
+                const single_leaderboard = fillLeaderboardData(data, TurnamentType.Single);
+
+                let leaderboard = "";
+
+                // Rating
+                leaderboard += "## Bewertung (Einzel- und Teamturniere)\n\n";
+                leaderboard += `1. Platz: ${formatRating(data.rating.firstPlace)}\n`;
+                leaderboard += `2. Platz: ${formatRating(data.rating.secondPlace)}\n`;
+                leaderboard += `3. Platz: ${formatRating(data.rating.thirdPlace)}\n`;
+
+                leaderboard += "\n## Bestenliste (Team)\n";
+                leaderboard += getLeaderboardMarkdown(team_leaderboard);
+
+                leaderboard += "\n## Bestenliste (Einzel)\n";
+                leaderboard += getLeaderboardMarkdown(single_leaderboard);
+
+                leaderboard += "\n## Turniere und Platzierungen (Team und Einzel)\n";
+                leaderboard += getTurnamentMarkdown(data.turnaments);
+
+                return markdown.replace("{{leaderboard}}", leaderboard);
+            }
+        );
+    };
+
+    // Add plugin to docsify's plugin array
+    $docsify = $docsify || {};
+    $docsify.plugins = [].concat($docsify.plugins || [], leaderboardPlugin);
+})();
+
+const TurnamentType = {
+    Single: "single",
+    Team: "team"
+};
+
+/**
+ * @param {number} rating 
+ */
+function formatRating(rating) {
+    return `${rating} Punkt${rating > 1 ? "e" : ""}`;
+}
+
+/**
+ * @param {{rating: {firstPlace: number, secondPlace: number, thirdPlace: number}, players: {name: string, singleParticipations: number, teamParticipations: number}[], turnaments: {type: string, firstPlace: string[], secondPlace: string[], thirdPlace: string[]}[]}} data 
+ * @param {TurnamentType} type
+ */
+function fillLeaderboardData(data, type) {
+    /**
+     * @type {{name: string, ppg: number, points: number, wins: number, participations: number}[]}
+     */
+    let leaderboard = [];
+
+    // Fill leaderboard with default values
+    for (const player of data.players) {
+        leaderboard.push({
+            name: player.name,
+            ppg: 0,
+            points: 0,
+            wins: 0,
+            participations: type === TurnamentType.Team ? player.teamParticipations : player.singleParticipations,
+        });
+    }
+
+    // Add points to players
+    for (const turnament of data.turnaments) {
+        if (turnament.type !== type) {
+            continue;
+        }
+
+        addPointsToPlaces(leaderboard, turnament.firstPlace, data.rating.firstPlace, true);
+        addPointsToPlaces(leaderboard, turnament.secondPlace, data.rating.secondPlace);
+        addPointsToPlaces(leaderboard, turnament.thirdPlace, data.rating.thirdPlace);
+    }
+
+    return leaderboard;
+}
+
+/**
+ * @param {{name: string, ppg: number, points: number, wins: number, participations: number}[]} leaderboard 
+ * @param {string[]} placePlayers 
+ * @param {number} rating 
+ * @param {boolean} isWin
+ */
+function addPointsToPlaces(leaderboard, placePlayers, rating, isWin = false) {
+    for (const firstPlacePlayer of placePlayers) {
+        const match = leaderboard.find((player) => player.name == firstPlacePlayer);
+        if (match == undefined) {
+            console.error(`Couldn't find player '${firstPlacePlayer}'`);
+            continue;
+        }
+
+        match.points += rating;
+        match.wins += isWin ? 1 : 0;
+        match.ppg = match.points / match.participations;
+    }
+}
+
+/**
+ * @param {{name: string, ppg: number, points: number, wins: number, participations: number}[]} leaderboard 
+ */
+function getLeaderboardMarkdown(leaderboard) {
+    const headers = [
+        "Name",
+        "PPG",
+        "Punkte",
+        "Siege",
+        "Teilnahmen"
+    ];
+
+    let markdown = `\n${headers.join(" | ")}\n`;
+    markdown += Array(headers.length).fill("--").join(" | ") + "\n";
+
+    leaderboard = leaderboard.filter((entry) => entry.participations > 0);
+    leaderboard.sort(function (a, b) {
+        // High PPG, high wins, high points, low participations
+
+        const ppgComparision = b.ppg - a.ppg;
+        if (ppgComparision != 0) {
+            return ppgComparision;
+        }
+
+        const winsComparision = b.wins - a.wins;
+        if (winsComparision != 0) {
+            return winsComparision;
+        }
+
+        const pointsComparision = b.points - a.points;
+        if (pointsComparision != 0) {
+            return pointsComparision;
+        }
+
+        const participationsComparision = a.participations - b.participations;
+        if (participationsComparision != 0) {
+            return participationsComparision;
+        }
+
+        return 0;
+    });
+
+    for (const entry of leaderboard) {
+        markdown += [
+            entry.name,
+            entry.ppg.toLocaleString("de-DE", { maximumFractionDigits: 2 }),
+            entry.points,
+            entry.wins,
+            entry.participations
+        ].join(" | ") + "\n";
+    }
+
+    return markdown;
+}
+
+/**
+ * @param {{date: string, flavor: string, firstPlace: string[], secondPlace: string[], thirdPlace: string[]}[]} turnaments 
+ */
+function getTurnamentMarkdown(turnaments) {
+    const headers = [
+        "Datum",
+        "1.",
+        "2.",
+        "3.",
+        "Flavor"
+    ];
+
+    let markdown = `\n${headers.join(" | ")}\n`;
+    markdown += Array(headers.length).fill("--").join(" | ") + "\n";
+
+    turnaments.sort(function (a, b) {
+        return Date.parse(a.date) - Date.parse(b.date);
+    });
+
+    for (const entry of turnaments) {
+        const date = new Date(Date.parse(entry.date));
+        markdown += [
+            date.toLocaleDateString("de-DE", { year: "2-digit", month: "2-digit", day: "2-digit" }),
+            entry.firstPlace.join(" & "),
+            entry.secondPlace.join(" & "),
+            entry.thirdPlace.join(" & "),
+            entry.flavor
+        ].join(" | ") + "\n";
+    }
+
+    return markdown;
+}
---
+++ docs/leaderboard-plugin/turnaments.json.js
@@ -0,0 +1,181 @@
+const turnamentData = {
+    "rating": {
+        "firstPlace": 5,
+        "secondPlace": 3,
+        "thirdPlace": 1
+    },
+    "players": [
+        {
+            "name": "Dominik",
+            "singleParticipations": 2,
+            "teamParticipations": 1
+        },
+        {
+            "name": "Sophie",
+            "singleParticipations": 1,
+            "teamParticipations": 1
+        },
+        {
+            "name": "Tim",
+            "singleParticipations": 0,
+            "teamParticipations": 2
+        },
+        {
+            "name": "Michael",
+            "singleParticipations": 0,
+            "teamParticipations": 2
+        },
+        {
+            "name": "Leonhard",
+            "singleParticipations": 2,
+            "teamParticipations": 2
+        },
+        {
+            "name": "Moritz W.",
+            "singleParticipations": 4,
+            "teamParticipations": 2
+        },
+        {
+            "name": "Mukhtar",
+            "singleParticipations": 4,
+            "teamParticipations": 2
+        },
+        {
+            "name": "Markus",
+            "singleParticipations": 2,
+            "teamParticipations": 2
+        },
+        {
+            "name": "Marcel",
+            "singleParticipations": 1,
+            "teamParticipations": 2
+        },
+        {
+            "name": "Felix",
+            "singleParticipations": 3,
+            "teamParticipations": 2
+        },
+        {
+            "name": "Lea",
+            "singleParticipations": 1,
+            "teamParticipations": 2
+        },
+        {
+            "name": "Millane",
+            "singleParticipations": 4,
+            "teamParticipations": 1
+        },
+        {
+            "name": "Sebbi",
+            "singleParticipations": 0,
+            "teamParticipations": 1
+        },
+        {
+            "name": "Moritz G.",
+            "singleParticipations": 2,
+            "teamParticipations": 0
+        },
+        {
+            "name": "Hartmut",
+            "singleParticipations": 1,
+            "teamParticipations": 0
+        },
+        {
+            "name": "Ines",
+            "singleParticipations": 1,
+            "teamParticipations": 0
+        },
+    ],
+    "turnaments": [
+        {
+            "date": "2023-01-28",
+            "flavor": "Moritz",
+            "type": "single",
+            "firstPlace": [
+                "Millane"
+            ],
+            "secondPlace": [
+                "Moritz G."
+            ],
+            "thirdPlace": [
+                "Leonhard"
+            ]
+        },
+        {
+            "date": "2023-02-10",
+            "flavor": "Moritz",
+            "type": "team",
+            "firstPlace": [
+                "Tim",
+                "Michael"
+            ],
+            "secondPlace": [
+                "Sophie",
+                "Moritz W."
+            ],
+            "thirdPlace": [
+                "Marcel",
+                "Felix"
+            ]
+        },
+        {
+            "date": "2023-03-05",
+            "flavor": "Moritz",
+            "type": "team",
+            "firstPlace": [
+                "Dominik",
+                "Leonhard"
+            ],
+            "secondPlace": [
+                "Markus",
+                "Mukhtar"
+            ],
+            "thirdPlace": [
+                "Lea",
+                "Moritz W."
+            ]
+        },
+        {
+            "date": "2023-04-23",
+            "flavor": "Moritz",
+            "type": "single",
+            "firstPlace": [
+                "Markus"
+            ],
+            "secondPlace": [
+                "Moritz G."
+            ],
+            "thirdPlace": [
+                "Moritz W."
+            ]
+        },
+        {
+            "date": "2023-06-22",
+            "flavor": "Moritz",
+            "type": "single",
+            "firstPlace": [
+                "Millane"
+            ],
+            "secondPlace": [
+                "Moritz W."
+            ],
+            "thirdPlace": [
+                "Felix"
+            ]
+        },
+        {
+            "date": "2023-06-22",
+            "flavor": "Moritz + 3D",
+            "type": "single",
+            "firstPlace": [
+                "Moritz W."
+            ],
+            "secondPlace": [
+                "Dominik"
+            ],
+            "thirdPlace": [
+                "Millane"
+            ]
+        }
+    ]
+};
