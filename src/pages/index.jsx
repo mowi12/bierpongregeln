641016ea4d657d62a556a9189c4d7a7b15bb1382
Moritz Wieland <moritz.wieland@googlemail.com>
2023-08-06 21:30:11 +0200

    feat: Change background colour to better match the logo

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,13 +13,10 @@ function HomepageHeader() {
         <header className={clsx('hero hero--primary', styles.heroBanner)}>
             <div className="container">
                 <img src={Logo} alt="Logo" width="300px" height="300px" />
-                <h1 className="hero__title">{siteConfig.title}</h1>
+                <h1 className={styles.heroTitle}>{siteConfig.title}</h1>
                 <p className="hero__subtitle">{siteConfig.tagline}</p>
                 <div className={styles.buttons}>
-                    <Link
-                        className="button button--secondary button--lg"
-                        to="/docs/regelwerk"
-                    >
+                    <Link className="button button--secondary button--lg" to="/docs/regelwerk">
                         Regelwerk
                     </Link>
                 </div>
