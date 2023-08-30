f377b5d7b11562bc59dda34b281c2842c69caf00
mowi12 <81433802+mowi12@users.noreply.github.com>
2023-08-30 21:09:00 +0200

    feat: Better color theme (#33)
    * feat: Change background color to better match the logo
    
    * chore: Bump version number
    
    * refactor: Defined new variable for home screen banner color
    
    * feat: Changed colors to match background
    
    * feat: revoke part of changes from commit 641016
    
    ---------
    
    Co-authored-by: Felix Schlegel <felix.schlegel@gmx.de>

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,16 +10,13 @@ import styles from './index.module.css';
 function HomepageHeader() {
     const { siteConfig } = useDocusaurusContext();
     return (
-        <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <header className={clsx('hero', styles.heroBanner, styles.homePageBackground)}>
             <div className="container">
                 <img src={Logo} alt="Logo" width="300px" height="300px" />
                 <h1 className="hero__title">{siteConfig.title}</h1>
                 <p className="hero__subtitle">{siteConfig.tagline}</p>
-                <div className={styles.buttons}>
-                    <Link
-                        className="button button--secondary button--lg"
-                        to="/docs/regelwerk"
-                    >
+                <div>
+                    <Link className="button button--secondary button--lg" to="/docs/regelwerk">
                         Regelwerk
                     </Link>
                 </div>
