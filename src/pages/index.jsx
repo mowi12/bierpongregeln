848a15f0a8480bacb34a2a0b375583188a71b977
Moritz Wieland <moritz.wieland@googlemail.com>
2023-08-30 15:31:16 +0200

    feat: revoke part of changes from commit 641016

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,7 +13,7 @@ function HomepageHeader() {
         <header className={clsx('hero', styles.heroBanner, styles.homePageBackground)}>
             <div className="container">
                 <img src={Logo} alt="Logo" width="300px" height="300px" />
-                <h1 className={styles.heroTitle}>{siteConfig.title}</h1>
+                <h1 className="hero__title">{siteConfig.title}</h1>
                 <p className="hero__subtitle">{siteConfig.tagline}</p>
                 <div>
                     <Link className="button button--secondary button--lg" to="/docs/regelwerk">
