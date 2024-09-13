7e6ad4ce203b393bb00dfca6068e2ae105793449
Moritz Wieland <moritz.wieland@googlemail.com>
2024-09-13 18:08:34 +0200

    chore: Remove docs versioning
    Remove docs versioning because it is not necessary for the localization process

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -122,10 +122,6 @@ const config: Config = {
                     position: "left",
                 },
                 {
-                    type: "docsVersionDropdown",
-                    position: "right",
-                },
-                {
                     type: "localeDropdown",
                     position: "right",
                 },
