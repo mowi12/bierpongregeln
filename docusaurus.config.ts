8916ebfecda64b11841e6e9c259b4fdea25a3ef4
Moritz Wieland <moritz.wieland@googlemail.com>
2024-09-13 16:53:53 +0200

    chore: Setup and configure for localization support
    Configure config, and dropdown menus

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -39,7 +39,19 @@ const config: Config = {
     // to replace "en" with "zh-Hans".
     i18n: {
         defaultLocale: "de",
-        locales: ["de"],
+        locales: ["de", "en"],
+        localeConfigs: {
+            de: {
+                label: "Deutsch",
+                direction: "ltr",
+                htmlLang: "de",
+            },
+            en: {
+                label: "English",
+                direction: "ltr",
+                htmlLang: "en",
+            },
+        },
     },
 
     presets: [
@@ -113,6 +125,14 @@ const config: Config = {
                     to: "docs/tournament_mode_generator",
                     label: "Turniermodus-Generator",
                     position: "left",
+                },
+                {
+                    type: "docsVersionDropdown",
+                    position: "right",
+                },
+                {
+                    type: "localeDropdown",
+                    position: "right",
                 },
                 {
                     href: "https://github.com/mowi12/bierpongregeln",
