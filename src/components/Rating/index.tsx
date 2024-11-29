13a110230f9ccf7e7d77fae2633c3c607dce2a7d
Moritz Wieland <moritz.wieland@googlemail.com>
2024-11-29 09:19:26 +0100

    feat: Translate rating component

--- src/components/Rating/index.tsx
+++ src/components/Rating/index.tsx
@@ -1,17 +1,29 @@
 import { usePluginData } from "@docusaurus/useGlobalData";
 import { LeadboardData } from "@site/src/types/leaderboard";
-
-function formatRating(rating: number) {
-    return `${rating} Punkt${rating > 1 ? "e" : ""}`;
-}
+import Translate from "@docusaurus/Translate";
 
 export default function Rating() {
     const { rating } = usePluginData("leaderboard-plugin") as LeadboardData;
     return (
         <div>
-            <div>{`1. Platz: ${formatRating(rating.firstPlace)}`}</div>
-            <div>{`2. Platz: ${formatRating(rating.secondPlace)}`}</div>
-            <div>{`3. Platz: ${formatRating(rating.thirdPlace)}`}</div>
+            <div>
+                <Translate id="rating.firstPlace" description="The label for the first place in a ranking" values={{points: rating.firstPlace}}>
+                    {"1. Platz: {points} Punkte"}
+                </Translate>
+                {"\u00A0"}
+            </div>
+            <div>
+                <Translate id="rating.secondPlace" description="The label for the second place in a ranking" values={{points: rating.secondPlace}}>
+                    {"2. Platz: {points} Punkte"}
+                </Translate>
+                {"\u00A0"}
+            </div>
+            <div>
+                <Translate id="rating.thirdPlace" description="The label for the third place in a ranking" values={{points: rating.thirdPlace}}>
+                    {"3. Platz: {points} Punkte"}
+                </Translate>
+                {"\u00A0"}
+            </div>
         </div>
     );
 }
