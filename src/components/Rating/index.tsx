125971cb034835c6fbd9c731566d45865b7e71fb
Moritz Wieland <moritz.wieland@googlemail.com>
2024-12-05 09:54:53 +0100

    refactor: Remove br as variable from rating translation

--- src/components/Rating/index.tsx
+++ src/components/Rating/index.tsx
@@ -6,24 +6,29 @@ export default function Rating() {
     const { rating } = usePluginData("leaderboard-plugin") as LeadboardData;
     return (
         <div>
-            <div>
-                <Translate id="rating.firstPlace" description="The label for the first place in a ranking" values={{points: rating.firstPlace}}>
-                    {"1. Platz: {points} Punkte"}
-                </Translate>
-                {"\u00A0"}
-            </div>
-            <div>
-                <Translate id="rating.secondPlace" description="The label for the second place in a ranking" values={{points: rating.secondPlace}}>
-                    {"2. Platz: {points} Punkte"}
-                </Translate>
-                {"\u00A0"}
-            </div>
-            <div>
-                <Translate id="rating.thirdPlace" description="The label for the third place in a ranking" values={{points: rating.thirdPlace}}>
-                    {"3. Platz: {points} Punkte"}
-                </Translate>
-                {"\u00A0"}
-            </div>
+            <Translate
+                id="rating.firstPlace"
+                description="The label for the first place in a ranking"
+                values={{ points: rating.firstPlace }}
+            >
+                {"1. Platz: {points} Punkte"}
+            </Translate>
+            <br />
+            <Translate
+                id="rating.secondPlace"
+                description="The label for the second place in a ranking"
+                values={{ points: rating.secondPlace }}
+            >
+                {"2. Platz: {points} Punkte"}
+            </Translate>
+            <br />
+            <Translate
+                id="rating.thirdPlace"
+                description="The label for the third place in a ranking"
+                values={{ points: rating.thirdPlace }}
+            >
+                {"3. Platz: {points} Punkte"}
+            </Translate>
         </div>
     );
 }
