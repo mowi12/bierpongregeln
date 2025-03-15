f97848a0a360ab8f2e718a5992b7213f374ddf15
Moritz Wieland <moritz.wieland@googlemail.com>
2025-03-15 22:18:19 +0100

    chore: Remove everything

--- src/components/Highlight/index.tsx
+++
@@ -1,8 +0,0 @@
-interface HighlightProps {
-    children: React.ReactNode;
-}
-
-export default function Highlight(props: HighlightProps) {
-    const { children } = props;
-    return <span style={{ color: "rgb(253, 119, 110)" }}>{children}</span>;
-}
