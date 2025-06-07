53ef3a2722bff17995884b1469e4c05d14f7ca03
Moritz Wieland <moritz.wieland@googlemail.com>
2025-06-07 23:25:37 +0200

    WIP

---
+++ src/lib/components/composites/navbar.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import * as NavigationMenu from "$lib/components/ui/navigation-menu/index.js";
+</script>
+
+<NavigationMenu.Root>
+  <NavigationMenu.List>
+    <NavigationMenu.Item>
+      <NavigationMenu.Trigger>Item One</NavigationMenu.Trigger>
+      <NavigationMenu.Content>
+        <NavigationMenu.Link>Link</NavigationMenu.Link>
+      </NavigationMenu.Content>
+    </NavigationMenu.Item>
+  </NavigationMenu.List>
+</NavigationMenu.Root>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Navbar from "$lib/components/composites/navbar.svelte";
+
+</script>
+<svelte:head>
+  <title>Bierpongregeln</title>
+</svelte:head>
+<main>
+  <Navbar />
+  <h1>Bierpongregeln</h1>
+</main>
