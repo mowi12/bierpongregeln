f7fe67fdb7f2898cbd576fe6bc5d7743e1b69ce3
Moritz Wieland <moritz.wieland@googlemail.com>
2025-06-07 21:49:15 +0200

    WIP

--- app/components/flavorPreamble.tsx
+++ app/components/flavorPreamble.tsx
@@ -1,20 +1,40 @@
+import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
+
 function FlavorPreamble() {
   return (
-    <div>
-      <span>Viel Spaß mit diesem Flavor!</span>
-      <br />
-      <span>Zu beachten ist, dass </span>
-      <a href="../regelwerk#flavors">Flavors</a>
-      <span>
-        {" "}
-        die Regeln des zugrunde liegenden Regelwerks abändern, entfernen{" "}
-      </span>
-      <span>und/oder um neue Regeln erweitern.</span>
-      <br />
-      <span>Es empfiehlt sich, vorher das </span>
-      <a href="../regelwerk">Regelwerk</a>
-      <span> durchzulesen.</span>
-    </div>
+    // <div className="mb-5">
+    //   <span>Viel Spaß mit diesem Flavor!</span>
+    //   <br />
+    //   <span>Zu beachten ist, dass </span>
+    //   <a href="../regelwerk#flavors">Flavors</a>
+    //   <span>
+    //     {" "}
+    //     die Regeln des zugrunde liegenden Regelwerks abändern, entfernen{" "}
+    //   </span>
+    //   <span>und/oder um neue Regeln erweitern.</span>
+    //   <br />
+    //   <span>Es empfiehlt sich, vorher das </span>
+    //   <a href="../regelwerk">Regelwerk</a>
+    //   <span> durchzulesen.</span>
+    // </div>
+    <Alert className="mb-5 border-0 border-l-4 border-callout">
+      <AlertTitle>Viel Spaß mit diesem Flavor!</AlertTitle>
+      <AlertDescription>
+        <p className="mb-0">
+          <span>Zu beachten ist, dass </span>
+          <a href="../regelwerk#flavors">Flavors</a>
+          <span>
+            {" "}
+            die Regeln des zugrunde liegenden Regelwerks abändern, entfernen
+            und/oder um neue Regeln erweitern.
+          </span>
+          <br />
+          <span>Es empfiehlt sich, vorher das </span>
+          <a href="../regelwerk">Regelwerk</a>
+          <span> durchzulesen.</span>
+        </p>
+      </AlertDescription>
+    </Alert>
   );
 }
 
--- app/components/highlight.tsx
+++ app/components/highlight.tsx
@@ -4,5 +4,5 @@ interface HighlightProps {
 
 export default function Highlight(props: HighlightProps) {
   const { children } = props;
-  return <span style={{ color: "rgb(253, 119, 110)" }}>{children}</span>;
+  return <span style={{ color: "rgb(0, 200, 110)" }}>{children}</span>;
 }
