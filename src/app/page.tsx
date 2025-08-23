9d9da41f8844eaef86354e0b7d5e23228a976481
Moritz Wieland <moritz.wieland@googlemail.com>
2025-08-23 16:08:11 +0200

    feat: Update hero logo size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const heroLogoSrc = "/logo.svg";
 export default function Home() {
     return (
         <div className="flex min-h-[70vh] flex-col items-center justify-center md:h-full">
-            <HeroLogo src={heroLogoSrc} className="mx-auto w-70 md:w-80" />
+            <HeroLogo src={heroLogoSrc} className="mx-auto w-70 md:w-[35%] lg:w-[26%]" />
             <h1 className="mt-8 text-5xl font-bold">Bierpongregeln</h1>
             <p className="text-muted-foreground mt-4 text-xl">Die beste Bierpongregelsammlung!</p>
         </div>
